2fe909d19fda5ab90089515c3acf4c1b14dc4d75
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2025-07-31 10:55:52 -0400

    documented colors.rs and tried out makefile in Rust

--- examples/cli-utils/src/colors.rs
+++ examples/cli-utils/src/colors.rs
@@ -15,22 +15,52 @@ pub fn red(s: &str) -> String {
     format!("\x1b[31m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code for green.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", green("Green"));
+/// ```
 pub fn green(s: &str) -> String {
     format!("\x1b[32m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code for blue.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", blue("Blue"));
+/// ```
 pub fn blue(s: &str) -> String {
     format!("\x1b[34m{}\x1b[0m", s)
 }
 
+/// Returns a string with the ANSI escape code for bold.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", bold("text in bold"));
+/// ```
 pub fn bold(s: &str) -> String {
     format!("\x1b[1m{}\x1b[0m", s)
 }
 
+/// Returns a string reset to default terminal color and format.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::*;
+/// println!("{}", reset("Dirty text"));
+/// ```
 pub fn reset(s: &str) -> String {
     format!("\x1b[0m{}\x1b[0m", s)
 }
 
+/// Represents a color that can be used to colorize strings in the terminal.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::Color;
+/// let color = Color::Red;
+/// ```
 pub enum Color{
     Red,
     Green,
@@ -38,12 +68,27 @@ pub enum Color{
     Bold,
 }
 
+/// Represents a colorized string with a specific color and the colorized output.
 pub struct ColorString {
     pub color: Color,
     pub string: String,
     pub colorized: String
 }
 
+
+/// Implements methods for the `ColorString` struct to handle colorization.
+/// # Examples:
+/// ```
+/// use cli_utils::colors::{Color, ColorString};
+/// let mut color_string = ColorString {
+///    color: Color::Red,
+///    string: "Hello".to_string(),
+///    colorized: String::new(),
+/// };
+/// 
+/// color_string.paint();
+/// println!("{}", color_string.colorized);
+/// ```
 impl ColorString {
     // create a method that will use the string and color fields to create a colorized string and assign it to the colorized field
     pub fn paint(&mut self) {
